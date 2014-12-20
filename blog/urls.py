8295a72bc5f3a34d99fcf500c4519f4d18d6f7fd
Snowman <python8taabo@gmail.com>
2014-12-20 22:48:00 +0900

    Added more views to the website

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(created__isnull=False).order_by('created')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
