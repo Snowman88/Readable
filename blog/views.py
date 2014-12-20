128c8aa15a2dcb3307bcc70ff16e6be7b31ce7b6
Snowman <python8taabo@gmail.com>
2014-12-20 22:49:47 +0900

    format properly

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def post_list(request):
     posts = Post.objects.filter(created__isnull=False).order_by('created')
     return render(request, 'blog/post_list.html', {'posts': posts})
