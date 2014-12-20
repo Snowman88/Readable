223ca6b9ad5bf7344178a20bd26ab951a20bd703
Snowman <python8taabo@gmail.com>
2014-12-20 16:15:40 +0900

    templates

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created__isnull=False).order_by('created')
+    return render(request, 'blog/post_list.html', {'posts': posts})
