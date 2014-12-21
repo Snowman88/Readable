920e1ab77ce13931251a16136db570d634ab29fa
Snowman <python8taabo@gmail.com>
2014-12-21 14:48:36 +0900

    Delete function

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
